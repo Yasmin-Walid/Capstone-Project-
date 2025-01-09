42b610696ee3c5cc4c8c3bf42b1aaf7690008a46
Yasmin-Walid <yasmin.elsnossy@gmail.com>
2025-01-09 17:47:17 +0200

    correcting the line     "build": "vite build",     "lint": "eslint .",     "preview": "vite preview"     in package.json

--- movie-database/src/App.jsx
+++ movie-database/src/App.jsx
@@ -1,10 +1,7 @@
 import "./App.css";
-import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider} from "react-router";
-import Landing from "./components/Pages/Landing"
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Landing from "./components/Pages/Landing";
 import MoviePage from "./components/Pages/MoviePage";
-
-const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -12,15 +9,11 @@ const router = createBrowserRouter([
     element: <Landing />,
   },
 
- { path: "/movie/:id",
-  element: <MoviePage />,
-}, 
+  { path: "/movie/:id", element: <MoviePage /> },
 ]);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
